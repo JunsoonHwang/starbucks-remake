142902e766390130cfd5e8b0f9979e20f0879051
JunsoonHwang <wnstns13@gmail.com>
2021-06-29 21:21:46 +0900

    비주얼 파트

--- js/main.js
+++ js/main.js
@@ -20,9 +20,15 @@ const badgeEl = document.querySelector('header .badges')
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY)
-  if(scrollY > 500) {
-    badgeEl.style.display = 'none'
+  if(window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
   } else {
-    badgeEl.style.display = 'block'
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
   }
 },300))
